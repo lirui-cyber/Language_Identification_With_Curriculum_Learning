3c22f1ec290843988bc70010d962016679614414
lirui <lruicor@163.com>
2023-01-08 15:56:38 +0800

    init

--- test_xsa_curriculum_learning.py
+++ test_xsa_curriculum_learning.py
@@ -1,6 +1,6 @@
-from model.model import *
-from model.data_load import *
-from score import scoring as scoring
+from model import *
+from data_load import *
+import scoring
 import subprocess
 import json
 # data/lre17_eval_3s_10_snrs data/lre17_eval_3s_15_snrs data/lre17_eval_3s_20_snrs data/lre17_eval_10s_5_snrs data/lre17_eval_10s_10_snrs data/lre17_eval_10s_15_snrs data/lre17_eval_10s_20_snrs data/lre17_eval_30s_5_snrs
@@ -45,7 +45,7 @@ def validation(valid_txt, valid_feats, model, model_name, device, kaldi, log_dir
     scoring.get_trials(valid_txt, num_lang, trial_txt)
     scoring.get_score(valid_txt, scores, num_lang, score_txt)
     eer_txt = trial_txt.replace('trial', 'eer')
-    subprocess.call(f"bash ./score/computeEER.sh "
+    subprocess.call(f"bash ./computeEER.sh "
                     f"--write-file {eer_txt} {trial_txt} {score_txt}", shell=True)
     cavg = scoring.compute_cavg(trial_txt, score_txt)
     print("Cavg: {:.2f} ".format(cavg))
@@ -56,7 +56,7 @@ def validation(valid_txt, valid_feats, model, model_name, device, kaldi, log_dir
 
 def main():
     parser = argparse.ArgumentParser(description='paras for making data')
-    parser.add_argument('--json', type=str, default='xsa_config.json')
+    parser.add_argument('--json', type=str, default='curriculum_config.json')
     args = parser.parse_args()
     with open(args.json, 'r') as json_obj:
         config_proj= json.load(json_obj)
@@ -65,7 +65,7 @@ def main():
         pass
     else:
         setup_seed(seed)
-    device = torch.device('cuda:{}'.format(config_proj["optim_config"]["device"])
+    device = torch.device('cuda'.format(config_proj["optim_config"]["device"])
                           if torch.cuda.is_available() else 'cpu')
     feat_dim = config_proj["model_config"]["d_k"]
     n_heads = config_proj["model_config"]["n_heads"]
@@ -81,7 +81,7 @@ def main():
                                     n_lang=config_proj["model_config"]["n_language"],
                                     max_seq_len=10000)
         model.load_state_dict(torch.load(config_proj["check_point"]))
-
+        print("test_model:{}".format(config_proj["check_point"]))
         model.to(device)
 
         model_name = config_proj["model_name"]
@@ -89,19 +89,22 @@ def main():
         kaldi_root = config_proj["kaldi"]
         if not os.path.exists(log_dir):
             os.mkdir(log_dir)
-
+        print("log_dir:{}".format(log_dir))
         feat_layer = config_proj["wav2vec_info"]["layer"]
 
         # need to change
-        test_sets = config_proj["Input"]["test_sets"].split()
+        test_sets = config_proj["Input"]["test_sets_10s_BCDFG"].split()
         for test in test_sets:
             # test = "data/lre17_eval_3s_5_snrs"
             print("## Test set: ", test)
-            test_txt = config_proj["Input"]["userroot"] + test + "/wav2vec_" + config_proj["wav2vec_info"]["model_name"] + ".txt"
-            test_feats = config_proj["Input"]["userroot"] + test +  "/wav2vec_" + config_proj["wav2vec_info"]["model_name"] + "_" + str(feat_layer) + "_layer/feats.scp"
+            exp_dir = log_dir  + "/" +test
+            if not os.path.exists(exp_dir):
+            	os.mkdir(exp_dir)
+            test_txt = config_proj["Input"]["userroot"] + "data-16k/" + test + "/wav2vec_" + config_proj["wav2vec_info"]["model_name"] + ".txt"
+            test_feats = config_proj["Input"]["userroot"] + "data-16k/" +test +  "/wav2vec_" + config_proj["wav2vec_info"]["model_name"] + "_" + str(feat_layer) + "_layer/feats.scp"
 
             if test_txt is not None:
-                validation(test_txt, test_feats, model, model_name, device, kaldi=kaldi_root, log_dir=log_dir,
+                validation(test_txt, test_feats, model, model_name, device, kaldi=kaldi_root, log_dir=exp_dir,
                     num_lang=config_proj["model_config"]["n_language"])
     else:
         print('Model check point for testing is not provided!!!')
